e867bf04ec7e017b07c718e56769650a4a1e7b2d
felipe-Santana1 <felipe,estudos666@outlook.com>
2022-04-19 16:58:32 -0300

    add Class Cliente, Class Cartao de Cŕedito

---
+++ POO_study/POO_study/CartaoCredito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_study
+{
+    public class CartaoCredito
+    {
+        public string Numero { get; set; }
+        public string DataValidade { get; set; }
+        public Cliente Cliente { get; set; }
+}
+}
