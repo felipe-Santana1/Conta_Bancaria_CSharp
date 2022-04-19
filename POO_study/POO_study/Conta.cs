f19a70e24fb43bb66593fc491518ae81dc204d11
felipe-Santana1 <felipe,estudos666@outlook.com>
2022-04-19 16:05:54 -0300

    Criação de Metodos / Apresentando Dados

---
+++ POO_study/POO_study/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_study
+{
+    public class Conta
+    {
+        public Conta(int numero)
+        {
+           this.Numero = numero;
+        }
+
+        private double Saldo { get;  set; }
+        public double Limite { get; set; }
+        public int Numero { get; private set; }
+
+        //metodos//
+
+        public void Deposita(double valor) { 
+            this.Saldo  += valor;
+        }
+
+        public bool Saca(int valor) {
+
+            double saldoDisponivel = Retorna_Saldo_Disponivel();
+            if (valor > saldoDisponivel)
+            {
+                Console.WriteLine("Saque indisponivel, valor exede o limite disponivel");
+                return false;
+            }
+           
+            this.Saldo -=valor;
+            return true;
+        }
+
+        public void AdicionarLimite(double valor) {
+            this.Limite = valor;
+        }  
+
+        public double Retorna_Saldo_Disponivel() { 
+        
+
+            return this.Saldo + this.Limite;
+        }
+    }
+}
---
+++ POO_study/POO_study/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace POO_study
+{
+    class Program
+    {
+        static void Main (String[]args)
+        {
+            Console.WriteLine("Aprendendo OOP");
+
+            //Criando Instancia da conta
+            Conta conta = new Conta(123);
+            conta.Limite = 500;
+
+           
+            //Limite da Conta
+            conta.AdicionarLimite(1500);
+
+            //Depositando
+            conta.Deposita(1500);
+            conta.Deposita(2500);
+            conta.Deposita(3500);
+
+            //Sacando valor
+           bool saca =  conta.Saca(2000);
+            if (saca)
+            {
+                double saldo = conta.Retorna_Saldo_Disponivel();
+                Console.WriteLine($"Numero da conta:{conta.Numero   }");
+                Console.WriteLine($"Seu saldo é R${saldo},00");
+                Console.WriteLine($"Seu saldo é R${conta.Limite},00");
+            }
+         
+        }
+    }
+}
